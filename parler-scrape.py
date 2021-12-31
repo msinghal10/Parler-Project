5d669be43565b8bf89f33787a03a76f0d6d988b0
Nihal Kumarswamy <kumarnihal04@gmail.com>
2021-12-31 15:36:30 -0600

    adding text to scrape multiple users

--- parler-scrape.py
+++ parler-scrape.py
@@ -17,26 +17,27 @@ Lightsofliberty
 -----------------------------5168501532397187871124942904--
 '''
 
-headers = {'Host': 'parler.com',
-'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
-'Accept': 'application/json, text/plain, */*',
-'Accept-Language': 'en-US,en;q=0.5',
-'Accept-Encoding': 'gzip, deflate, br',
-'Referer': 'https://parler.com/user/OANN',
-'Content-Type': 'multipart/form-data; boundary=---------------------------5168501532397187871124942904',
-'Content-Length': '282',
-'Origin': 'https://parler.com',
-'Connection': 'keep-alive',
-'Cookie': '_pk_id.1.4eb6=c537e2ceea449bd0.1639087612.; _jsuid=962679935; _pk_ses.1.4eb6=1; heatmaps_g2g_101336175=no; PHPSESSID=94d2gne692jp2nmd4u7qbt8fg2; parler_auth_token=fee0a0373a46dd1e705e9c63bdbc7e5c4d7e9928c55ed99e5b48eb7c03a0295a; _first_pageview=1', #Add cookies from browser 
-'Sec-Fetch-Dest': 'empty',
-'Sec-Fetch-Mode': 'no-cors',
-'Sec-Fetch-Site': 'same-origin',
-'Pragma': 'no-cache',
-'Cache-Control': 'no-cache'}
+
 
 def posting():
+	headers = {'Host': 'parler.com',
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
+	'Accept': 'application/json, text/plain, */*',
+	'Accept-Language': 'en-US,en;q=0.5',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Referer': 'https://parler.com/user/OANN',
+	'Content-Type': 'multipart/form-data; boundary=---------------------------5168501532397187871124942904',
+	'Content-Length': '282',
+	'Origin': 'https://parler.com',
+	'Connection': 'keep-alive',
+	'Cookie': '_pk_id.1.4eb6=c537e2ceea449bd0.1639087612.; _jsuid=962679935; _pk_ses.1.4eb6=1; heatmaps_g2g_101336175=no; PHPSESSID=94d2gne692jp2nmd4u7qbt8fg2; parler_auth_token=fee0a0373a46dd1e705e9c63bdbc7e5c4d7e9928c55ed99e5b48eb7c03a0295a; _first_pageview=1', #Add cookies from browser 
+	'Sec-Fetch-Dest': 'empty',
+	'Sec-Fetch-Mode': 'no-cors',
+	'Sec-Fetch-Site': 'same-origin',
+	'Pragma': 'no-cache',
+	'Cache-Control': 'no-cache'}
 	r = sesh.post('https://parler.com/pages/feed.php', data = dt, headers = headers)
-	# print(r.text)
+	
 time_now = time.time()
 
 for x in range(100):
