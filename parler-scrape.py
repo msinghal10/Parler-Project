3b60d3a6751c2e3ee0af0f592b0612ef3faa4f1c
Mohit Singhal <mohit.singhal@mavs.uta.edu>
2021-12-31 19:40:27 -0600

    log giles

--- parler-scrape.py
+++ parler-scrape.py
@@ -2,7 +2,7 @@ import requests, sys, threading, time
 from random import randint 
 from itertools import islice
 from csv import writer
-
+import json	
 #Scraping stuff
 op_file = 'data/'
 
@@ -57,7 +57,7 @@ def posting(username, iters):
 			log_list.append(username)
 			log_list.append(str(r.elapsed.total_seconds()))
 			log_list.append(str(r.status_code))
-			log_list.append(str(len(page)))
+			log_list.append(str(len(page[0])))
 			log_list.append(str(pg))
 			log_list.append(str(iters))
 			writer_obj = writer(f)
