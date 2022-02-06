717ef5592249dac5e21b19dcb9fbe20073ec7c6b
Mohit Singhal <mohit.singhal@mavs.uta.edu>
2022-02-06 13:06:19 -0600

    asd

---
+++ get_unique.py
@@ -0,0 +1,26 @@
+from os import listdir
+import json
+
+list_of_files = listdir('./data')
+
+for i in range(len(list_of_files)):
+	list_of_files[i] = './data/'+list_of_files[i]
+
+usernames = []
+
+total_files = len(list_of_files)
+file_current = 0
+
+for file in list_of_files:
+	with open(file) as f:
+		data = json.load(f)
+		for entry in data:
+			if entry['username'] in usernames:
+				continue
+			else:
+				usernames.append(entry['username'])
+				
+	print('Files finished %d'%file_current)
+	file_current += 1
+
+print(len(usernames))
